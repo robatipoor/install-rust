930bfcf61cfa81e2d06ff643011df25f16b5591c
mahdi <mahdi.robatipoor@gmail.com>
2019-04-18 11:08:24 +0430

    edit readme and add some command to install.sh

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
+## install-rust
+### for install rust and all tools run this command it
+```sh
+curl -LSs https://git.io/fjUal | bash -s all
+```
+### or install rust without tools  
+```sh
 curl -LSs https://git.io/fjUal | bash
+```
--- install.sh
+++ install.sh
@@ -12,18 +12,19 @@ rustup component add rust-analysis
 rustup component add llvm-tools-preview
 # *********** install vscode extension rust language **********
 if [ -x "$(command -v code)" ]; then
+    echo 'install rust vscode extension'
     code --install-extension rust-lang.rust
     code --install-extension polypus74.trusty-rusty-snippets
     code --install-extension serayuzgur.crates
 fi
 # ****************** install cargo subcommands ******************
+echo 'install cargo subcommands'
 cargo install cargo-fix
 cargo install cargo-bloat
 cargo install cargo-asm
 cargo install cargo-outdated
 cargo install cargo-edit
 cargo install cargo-audit
-cargo install cargo-env
 cargo install cargo-expand
 cargo install cargo-tree # or cargo-modules
 cargo install cargo-update
@@ -54,6 +55,7 @@ fi
 export RUST_SRC_PATH="$(rustc --print sysroot)/lib/rustlib/src/rust/src"
 # ****************************** other tools ***************************
 if [ "$1" == "all" ];then
+    echo 'install useful rust command line tools'
     cargo install bat # or fcat
     cargo install lsd # or exa
     cargo install fd-find
@@ -71,8 +73,11 @@ if [ "$1" == "all" ];then
     cargo install ffsend
     cargo install sd
     cargo install peep
+    cargo install pf
+    cargo install note-rs
 fi
 # ********************** set alias update rust command *******************
+echo 'add uprust alias for simple command for update rust'
 if [ -f "$HOME/.bashrc" ]; then
     echo 'alias uprust="rustup update && cargo install-update -a"' >> "$HOME/.bashrc"
 fi
